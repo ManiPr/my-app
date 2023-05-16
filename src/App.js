0ba7042694f111f4af2688f2408aa637631e256d
ManiGeek <maniweeb@gmail.com>
2023-05-17 00:12:04 +0330

    feat:start project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Header from './components/Header/Header'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 function App() {
   return (
     <Header></Header>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import './Header.css'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faInstagram} from '@fortawesome/free-brands-svg-icons'
 function Header(){
-    return (<div>
-       fd
-    </div>
+    return (
+    <header>
+      <div className='container'>
+        <div className='nav'>
+          <div className='nav__icons'>
+            <div className='nav__icon'><FontAwesomeIcon icon={faInstagram} /></div>
+            <div className='nav__icon'></div>
+            <div className='nav__icon'></div>
+          </div>
+          <ul className='nav__menus'>
+            <li className='nav__menu'></li>
+            <li className='nav__menu'>MENU</li>
+            <li className='nav__menu'>RESERVATIONS</li>
+            <li className='nav__menu'>NEWS</li>
+            <li className='nav__menu'>CONTACT</li>
+          </ul>
+          <div className='nav__logo'></div>
+        </div>
+      </div>
+    </header>
   );
 }
 export default Header;
